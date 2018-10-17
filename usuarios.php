913f303fc56876ebec697a238a6d74033d29f1c0
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-10-16 23:33:53 -0300

    Inclusão da classe alterar

--- usuarios.php
+++ usuarios.php
@@ -4,10 +4,30 @@
 <?php
     require_once('header.php');
     require_once('classes/usuario.php');
+
+    $id = $_POST["id"];
+    $senha = $_POST["senha_editar"];
+    if($id!=''&& $senha != '') {
+       
+        $user = new Usuario();
+        $cadastro_valido = $user->Editar_usuario($id, $senha);
+        if($cadastro_valido){
+            echo "<script>alert('Deu Bom');
+            window.location.href = 'usuarios.php';
+            </script>";
+            
+        }else{
+                echo "Algo deu errado";
+        }
+    
+    }else{
+        echo "Informe a nova senha";
+    }
+    
 ?>    
     <div class="container">
         <h4>
-            Usuarios cadastradas
+            Usuários cadastrados
         </h4>
     <?php
         $usuario = new Usuario();
@@ -19,8 +39,8 @@
             <table class='table table-borderless table-hover'>
                 <thead class="thead-light">
                     <tr>
+                        <th>#</th>
                         <th>Email</th>
-                        <th>Senha</th>
                         <th>Status</th>
                         <th>Log</th>
                         <th>Editar</th>
@@ -30,19 +50,63 @@
                 </thead>
                 <tbody class="table-striped">
         <?php
+
+            
             foreach ($usuarios as $valor){
+                  
                 echo "<tr>
+                        <td>".$valor["id"]."</td>
                         <td>".$valor['email']."</td>
-                        <td>".$valor['senha']."</td>
                         <td>".$valor['status']."</td>
                         <td>".$valor['log']."</td>
-                        <td><a href='#'>Editar</a></td>
+                        <td><a href='usuarios.php?id={$valor["id"]}&edit=1'>Editar</a></td>
                         <td><a href='#'>Excluir</a></td>
                     </tr>";
             }
-        }?>
+        ?>
+        <?php
+        $database = new Database();
+        if($_GET['edit']==1){
+        $sql = "SELECT * FROM usuario where id = {$_GET['id']}";
+        $result = $database->executar($sql);
+        $user = $result->fetch_assoc();
+        
+
+        ?>
+
+            <form action = "usuarios.php" method = "post">
+    <table>
+        <h2> Editar Senha</h2>
+        <tr>
+            <td>
+                <input value=<?echo $user['id']?> name='id' style="display:none"/>
+                <label>E-mail</label>
+                <label><?echo $user['email'];?></label>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+
+                <label>Senha</label>
+                <input type="password" name="senha_editar" />
+
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <input type="submit" name="botao_editar" value="Salvar" />
+            </td>
+        </tr>
+    </table>
+    </form>
+    <?php
+    } }
+    ?>
+       
                 </tbody>
             </table>
     </div>
 </div>
-<?php require_once('footer.php');
+<?php require_once('footer.php');?>
